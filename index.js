69eba32ec863167647562332e019dea15deff494
Anurag Khobragae <anuragk2204@gmail.com>
2024-12-18 23:24:15 +0530

    trial 2

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const app = express()
 app.get('/api',(req,res)=>{
     console.log('Hello')
     res.json({ message: 'Hello' })
-    res.send('Hello')
-    res.status(200)
 })
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
